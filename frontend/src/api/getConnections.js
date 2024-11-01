a081149b7a4a76ae6866fbb0472a54289db7063f
Chu Minseok <mchu7797@gmail.com>
2024-11-01 22:30:49 +0900

    [프론트] 구조 수정 및 코드 정리

--- frontend/src/api/getConnections.js
+++ frontend/src/api/getConnections.js
@@ -1,17 +1,9 @@
 export async function getConnections() {
     try {
-        const endpoint = window.location.hostname;
-        const response = await fetch(`http://${endpoint}:2024/connections`, {
-            method: 'GET',
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
+        const response = await fetch(`${window.location.origin}:2024/connections`);
         return await response.text();
     } catch (error) {
-        console.error('HTTP error! status:', error);
-        return "";
+        console.error('연결 오류:', error);
+        return '';
     }
 }
