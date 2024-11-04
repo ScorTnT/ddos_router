d02076b3cf410ede79775819ba2e8b82dfb79200
ScorTnT <flalxlemdkdlel@naver.com>
2024-11-04 12:13:27 +0900

    [프론트] api

--- frontend/src/api/userConfig.js
+++ frontend/src/api/userConfig.js
@@ -1,3 +1,14 @@
 export async function adminId() {
-    
+    try {
+        const response = await fetch(`http://${window.location.hostname}:2024/api/user`);
+
+        if (response.ok) {
+            return await response.json();
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
