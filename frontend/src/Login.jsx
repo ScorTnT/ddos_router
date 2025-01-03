69db5363f39fc3af38280446f5a61872df811072
ScorTnT <flalxlemdkdlel@naver.com>
2025-01-03 16:22:15 +0900

    [프론트] 로그인 동작 설정정

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -39,8 +39,8 @@ const Login = ({setIsLoggedIn}) => {
         setError('');
 
         try {
-            //const loginSuccess = await attemptLogin(formData.username, formData.password); // 이 둘을 절대 조심
-            const loginSuccess = true;                                                       // 위는 실제 서버에서 로그인 확인, 아래는 무조건 통과
+            const loginSuccess = await attemptLogin(formData.username, formData.password); // 이 둘을 절대 조심
+            // const loginSuccess = true;                                                       // 위는 실제 서버에서 로그인 확인, 아래는 무조건 통과
             if (loginSuccess) {
                 setIsLoggedIn(true);
             } else {
