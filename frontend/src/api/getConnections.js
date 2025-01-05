18068cb11d5cd6e1c173d677a50f57632bec53b6
GY <alex508@naver.com>
2025-01-05 22:10:06 +0900

    오류 수정2

--- frontend/src/api/getConnections.js
+++ frontend/src/api/getConnections.js
@@ -3,6 +3,7 @@ export async function getConnections() {
         const response = await fetch(`http://${window.location.hostname}:2024/connections`);
         //return await response.text();
         const data = await response.json();
+        return data;
     } catch (error) {
         console.error('연결 오류:', error);
         return '';
