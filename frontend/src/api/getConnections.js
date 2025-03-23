a47700845e0c68cb7022b52688351f2ed8528a59
Chu Minseok <mchu7797@gmail.com>
2025-03-23 16:31:56 +0900

    커넥션 정보가 안 받아와지는 오류 수정

--- frontend/src/api/getConnections.js
+++ frontend/src/api/getConnections.js
@@ -1,8 +1,15 @@
 export async function getConnections() {
     try {
         const response = await fetch(`http://${window.location.hostname}:2024/connections`);
-        console.log(response);
-        return await response.text();
+        
+        if (!response.ok) {
+            throw new Error("Failed to fetch router connection informations");
+        }
+        
+        const connections = await response.json();
+        console.log(connections);
+
+        return connections;
     } catch (error) {
         console.error('연결 오류:', error);
         return '';
