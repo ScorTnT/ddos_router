4b30f1ba5ff57c59c71b50d8be1a3a742539468a
Minseok Chu <mchu7797@gmail.com>
2025-04-02 15:02:18 +0900

    [백엔드] 코드 의존성 수정정

--- backend/utils/arp.go
+++ backend/utils/arp.go
@@ -13,6 +13,39 @@ type ARPEntry struct {
 	MAC string `json:"mac"`
 }
 
+func GetARPNowTable() ([]ARPEntry, error) {
+	cmd := exec.Command("awk", "{print $2, $3}", "/tmp/dhcp.leases")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute awk command: %v", err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(out.String()), "\n")
+	var entries []ARPEntry
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) != 2 {
+			continue
+		}
+
+		mac := fields[0]
+		ip := fields[1]
+
+		if isValidMAC(mac) {
+			entry := ARPEntry{
+				IP:  ip,
+				MAC: mac,
+			}
+			entries = append(entries, entry)
+		}
+	}
+
+	return entries, nil
+}
+
 // GetARPTable 함수 정의
 func GetARPTable() ([]ARPEntry, error) {
 	cmd := exec.Command("ip", "neigh")
