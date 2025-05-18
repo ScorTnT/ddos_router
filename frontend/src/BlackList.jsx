0ca105c7dd74d7193bff99420405237e5f7690b5
GY <alex508@naver.com>
2025-05-18 14:22:03 +0900

    [프론트] 제한목록 틀

--- frontend/src/BlackList.jsx
+++ frontend/src/BlackList.jsx
@@ -1,12 +1,61 @@
 import { useEffect } from "react";
-
+import {
+    Button,
+    Card,
+    CardContent,
+    TextField,
+    Stack,
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableCell,
+    Paper,
+    Typography,
+    Tooltip,
+    IconButton
+} from "@mui/material";
 function BlackList(){
-    
+    const [blockedIP, setBlockedIP] = useState([]);
     useEffect(() => {
+        if(blockedIP){
+            
+        }
     }, []);
-    return <>
-        black list
-        </>
-}
+    return (
+        <Stack spacing={3}>
+            <Card>
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        차단된 IP 목록
+                    </Typography>
+
+                    <TableContainer 
+                        component={Paper}
+                        sx={{
+                            overflowY : 'auto',
+                        }}
+                    >
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{ fontWeight: 'bold' }}>IP</TableCell>
+                                    <TableCell sx={{ fontWeight: 'bold' }}>차단된 시각</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>     </TableCell>
+                                    <TableCell>     </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </CardContent>
+            </Card>
+        </Stack>
+    )
+};
 
 export default BlackList;
+
