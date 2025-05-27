3ca765a7fa5cd1fbfc474b7cede8e3490bb43723
GY <alex508@naver.com>
2025-05-28 04:29:23 +0900

    [프론트] 제한목록 api 연동 내용 추가(진행중)

--- frontend/src/BlackList.jsx
+++ frontend/src/BlackList.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import {
     Button,
+    Box,
+    Button,
     Card,
     CardContent,
     TextField,
@@ -18,14 +20,59 @@ import {
 } from "@mui/material";
 import { getBlockedIP, blockIP, unblockIP } from './api/getBlockedIP';
 function BlackList(){
+    const [protectionLog, setProtectionLog] = useState([]);
     const [blockedIP, setBlockedIP] = useState([]);
     const [blackList, setBlackList] = useState([]);
     const [whiteList, setWhiteList] = useState([]);
-    useEffect(() => {
-        // if(){
+    const [isAutoUpdate, setIsAutoUpdate] = useState(true);
+    const [updateError, setUpdateError] = useState(null);
 
-        // }
+    /*
+    const fetchProtectionLog = async () => {
+        try {
+            const protectionData = await getBlockedIP();
+            if (Array.isArray(protectionData)) setProtectionLog(protectionData);
+            else setProtectionLog([]);
+
+        } catch (error) {
+            setUpdateError('제한 목록 업데이트 중 오류가 발생했습니다.');
+            console.error('BlackList update error:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProtectionLog();
     }, []);
+
+    useEffect(() => {
+        if (!isAutoUpdate) return;
+        const intervalId = setInterval(fetchProtectionLog, 5000);
+        return () => clearInterval(intervalId);       
+    }, [isAutoUpdate]);
+
+    useEffect(() => {
+        // if (!Array.isArray(protectionLog)) return;
+        const tempBlocked = [];
+        const tempBlack = [];
+        
+        protectionLog.forEach(pLog => {
+            const pLogIp = pLog.ip;
+            if (blackList.some(item => item.ip === pLogIp)) {
+                tempBlack.push();
+            } else if (whiteList.includes(ip)) {
+                tempWhite.push(ipObj);
+            } else {
+                tempBlocked.push(ipObj);
+            }
+        });
+
+        setBlockedIP(tempBlocked);
+        // setBlackList(tempBlack.map(i => i.ip)); // <- 필요하면
+        // setWhiteList(tempWhite.map(i => i.ip)); // <- 필요하면
+    }, [apiList, blackList, whiteList]);
+    
+    */
+
     return <>
         <Stack spacing={3}>
             <Card>
