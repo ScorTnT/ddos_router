a1b70114f2e9000dd121a7341f1d0e6e50ba2a7b
SangU <81074706+ScorTnT@users.noreply.github.com>
2025-09-20 17:11:51 +0900

    [snort] rule 예시 추가 및

--- server_/edit-setting/snort/append-to-snort.lua
+++ server_/edit-setting/snort/append-to-snort.lua
@@ -1,8 +1,9 @@
-ips = {
+ids = {
         mode = tap,
         rules = [[
-                alert icmp 192.168.3.225/32 any -> any any (msg:"INLINE ICMP"; flags:S; sid:1; rev:1;)
-                alert tcp 192.168.3.225/32 any -> any any (msg:"INLINE TCP"; flags:S; sid:2; rev:1;)
+                alert icmp 192.168.3.225/32 any -> any any (msg:"ICMP packet from attacker"; flags:S; sid:1; rev:1;)
+                alert tcp 192.168.3.225/32 any -> any any (msg:"SYN packet from attacker to any port"; flags:S; sid:2; rev:1;)
+                alert tcp 192.168.3.225/32 any -> any 80 (msg:"SYN packet from attacker"; flags:S; sid:3; rev:1;)
         ]]
 }
 
@@ -12,8 +13,7 @@ daq = {
         {
                 {
                         name = 'afpacket',
-                        mode = 'inline'
+                        mode = 'passive'
                 }
         }
 }
-
