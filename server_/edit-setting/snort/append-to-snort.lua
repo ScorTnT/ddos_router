996e4fedabe57ce93dfe0fa9ea2b4a260939d8c8
SangU <81074706+ScorTnT@users.noreply.github.com>
2025-09-20 15:11:37 +0900

    [server] snort rule setting (only the SYN flag is set)

--- server_/edit-setting/snort/append-to-snort.lua
+++ server_/edit-setting/snort/append-to-snort.lua
@@ -1,8 +1,8 @@
 ips = {
         mode = tap,
         rules = [[
-                alert icmp 192.168.3.225/32 any -> any any (msg:"INLINE ICMP"; sid:1; rev:1;)
-                alert tcp 192.168.3.225/32 any -> any any (msg:"INLINE TCP"; sid:2; rev:1;)
+                alert icmp 192.168.3.225/32 any -> any any (msg:"INLINE ICMP"; flags:S; sid:1; rev:1;)
+                alert tcp 192.168.3.225/32 any -> any any (msg:"INLINE TCP"; flags:S; sid:2; rev:1;)
         ]]
 }
 
