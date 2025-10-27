57b6d8fc373115cba529aad226e153d795784b7e
ScorTnT <sangu1115@naver.com>
2025-10-27 23:25:24 +0900

    [snort] snort.lua , alert rule setting fix .

--- server_/edit-setting/snort/append-to-snort.lua
+++ server_/edit-setting/snort/append-to-snort.lua
@@ -1,9 +1,9 @@
 ids = {
         mode = tap,
         rules = [[
-                alert icmp 192.168.3.225/32 any -> any any (msg:"ICMP packet from attacker"; flags:S; sid:1; rev:1;)
-                alert tcp 192.168.3.225/32 any -> any any (msg:"SYN packet from attacker to any port"; flags:S; sid:2; rev:1;)
-                alert tcp 192.168.3.225/32 any -> any 80 (msg:"SYN packet from attacker"; flags:S; sid:3; rev:1;)
+                alert icmp !192.168.2.0/24 any -> any any (msg:"non-LAN source"; flags:S; sid:1; rev:1;)
+                alert tcp !192.168.2.0/24 any -> any any (msg:"non-LAN source to any port"; flags:S; sid:2; rev:1;)
+                alert tcp !192.168.2.0/24 any -> any 80 (msg:"non-LAN source to port 80"; flags:S; sid:3; rev:1;)
         ]]
 }
 
@@ -17,3 +17,6 @@ daq = {
                 }
         }
 }
+
+
+
