46da810f83729adfd95d6ee6ddd8c959d4029f4d
ScorTnT <sangu1115@naver.com>
2025-11-13 15:53:37 +0900

    [프론트] dev:prod를 위한 환경변수  변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,5 +9,12 @@ export default defineConfig({
         port: 5173,
         host: '0.0.0.0',
         https: false, // 임시로 false로 설정
+        proxy: {
+            '/api': {
+                target: 'http://192.168.2.1',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
     }
 })
